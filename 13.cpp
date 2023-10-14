46ae4515e9a93bc5c57e5a110a08294df647b9cf
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-10-15 00:05:30 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+       int maxi = INT_MIN;
+        int prod=1;
+
+        for(int i=0;i<nums.size();i++)
+        {
+          prod*=nums[i];
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=nums[i];
+
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        return maxi;
+    }
+
+int main()
+{
+    vector<int> nums ={2,3,-2,4};
+    cout<<"Output: "<<maxProduct(nums)<<endl;
+    return 0;
+}
