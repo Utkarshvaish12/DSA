55a7a5baf060b7091c07d4216066a4e98b7c2131
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-10-16 20:52:54 +0530

    20.cpp

--- 20.cpp
+++ 20.cpp
@@ -1,16 +1,1 @@
-#include<iostream>
-#include<vector>
-#include<set>
-#include<bits/stdc++.h>
 
-using namespace std;
-
-
-
-int main()
-{
-vector <int> nums = {11, 15, 6, 8, 9, 10};
-int X = 161;
-cout<<"Output: "<<boolalpha<<find(nums,X)<<endl;
-    return 0;
-}
