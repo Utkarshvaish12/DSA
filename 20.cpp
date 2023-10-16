7ac645546e7160736ccb7c0ea8cde5802a8f0d61
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-10-16 20:49:44 +0530

    Add files via upload

---
+++ 20.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+vector <int> nums = {11, 15, 6, 8, 9, 10};
+int X = 161;
+cout<<"Output: "<<boolalpha<<find(nums,X)<<endl;
+    return 0;
+}
